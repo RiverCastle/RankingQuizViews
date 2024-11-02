1be53d929b675fde9a9d597fae313ef7ebc17c59
RiverCastle <rkdtjd12@gmail.com>
2024-11-02 17:08:27 +0900

    Feat: 카카오 로그인 기능 추가

--- main/resources/static/js/home/kakao-login.js
+++ main/resources/static/js/home/kakao-login.js
@@ -3,8 +3,10 @@ document.getElementById('kakaoLoginBtn').addEventListener('click', function() {
     fetch('/config')
         .then(response => response.json())
         .then(config => {
-            const clientId = config.clientId;
-            const redirectUri = config.redirectUri;
+//            const clientId = config.clientId;
+//            const redirectUri = config.redirectUri;
+            const clientId = '41ad8e26ac1e92bfcac84f788d229cef';
+            const redirect_uri = protocol + FRONTEND_BASE_URL + '/login-wait';
             const authUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code`;
             window.location.href = authUrl;
         })
