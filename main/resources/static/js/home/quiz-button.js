3aa6d71881402f12b1191776a924671b4fce7fad
RiverCastle <rkdtjd12@gmail.com>
2024-11-05 10:10:48 +0900

    Fix: Home 페이지 비회원 퀴즈 시작시 이름 누락
    - 이름 키값 수정 userName -> username
    - item-control-func 참조

--- main/resources/static/js/home/quiz-button.js
+++ main/resources/static/js/home/quiz-button.js
@@ -1,9 +1,9 @@
 document.getElementById('quizButton').addEventListener('click', function() {
-    const userName = document.getElementById('nameInput').value.trim();
+    const username = document.getElementById('nameInput').value.trim();
     if (userName === "") {
         alert("이름/별명을 입력해주세요.");
         return;
     }
-    sessionStorage.setItem('userName', userName);
+    sessionStorage.setItem('username', username);
     window.location.href = protocol + FRONTEND_BASE_URL + '/quiz';
 });
